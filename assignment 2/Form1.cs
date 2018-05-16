7324ec99f81b4867a9cbd6ca0da15226044b37cd
Sean Brown <rc0029451@CRO-ICF07-08N>
2018-05-16 13:56:36 +0100

    Add project files.

---
+++ assignment 2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment_2
+{
+    public partial class Form1 : Form
+    {
+        class row
+        {
+            double time;
+            double acceloration;
+            double velocity;
+            double altitude;
+        }
+        List<row> table = new List<row>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            OpenFileDialog1.Filter = "CSV Files|*.csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            { 
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+
+                }
+        }
+    }
+}
