76e1c90d9128de24e0a410c5f50f41cef32c1c0e
Sean Brown <rc0029451@CRO-ICF07-08N>
2018-06-13 14:06:50 +0100

    sfzdzxs\z\

--- assignment 2/Form1.cs
+++ assignment 2/Form1.cs
@@ -15,15 +15,35 @@ namespace assignment_2
     {
         class row
         {
-            double time;
-            double acceloration;
-            double velocity;
-            double altitude;
+            public double time;
+            public double current;
+            public double dcurrent;
+            public double charge;
         }
         List<row> table = new List<row>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void calculateCurrent()
+        {
+            for (int i = 1; 1 < table.Count; i++) 
+            {
+                double dQ = table[i].charge - table[i - 1].charge;
+                double dt = table[i].time - table[i - 1].time;
+                table[i].current = dQ / dt;
+            }
+        }
+
+        private void calculateCurrent()
+        {
+            for (int i = 1; 1 < table.Count; i++)
+            {
+                double dQ = table[i].charge - table[i - 1].charge;
+                double dt = table[i].time - table[i - 1].time;
+                table[i].current = dQ / dt;
+            }
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
@@ -32,26 +52,45 @@ namespace assignment_2
             openFileDialog1.Filter = "CSV Files|*.csv";
             DialogResult result = openFileDialog1.ShowDialog();
             if (result == DialogResult.OK)
+            {
                 try
                 {
-                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName)) ;
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
                     {
-                        string line = sr.ReadLine.();
+                        string line = sr.ReadLine();
                         while (!sr.EndOfStream)
                         {
                             table.Add(new row());
-                            string[] r = sr.ReadLine().Split(",");
+                            string[] r = sr.ReadLine().Split(',');
                             table.Last().time = double.Parse(r[0]);
                             table.Last().charge = double.Parse(r[1]);
                         }
                     }
+                    calculateCurrent();
+                } 
+                catch (IOException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "failed to open.");
                 }
-                
-            
+                catch (FormatException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "is not in the required format");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "is not in the required format");
+                }
+                catch
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "has rows that have the same time");
+                }
+            }
+        }
+    }
+}
 
 
-       
-    
+
 
 
 
